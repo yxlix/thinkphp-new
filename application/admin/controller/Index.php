5151264279d7a76d847caf3b94004b7912a9216d
SinyLi <yxlix1@163.com>
2018-12-15 21:08:48 +0800

    使用hi-admin框架完成首页搭建

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+class Index extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+    public function welcome(){
+        return '我的桌面';
+    }
+}
